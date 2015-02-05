181b9ea0f6efcc5ac2c1d9c53e9a0e5cd4fb473e
opielanaruto <opielanaruto@gmail.com>
2015-02-05 10:47:10 -0600

    https://github.com/opielanaruto/MyCodingExcercise.git

---
+++ !ITCodingExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _ITCodingExercise
+{
+	class Program
+	{
+		private static void Main()
+		{
+			var questions = File.ReadAllText("QuestionsToBeAnswered.txt");
+			var names = File.ReadAllText("IT Coding Excercise.txt").Split(',').Select(s => s.Trim('"')).ToArray();
+			Array.Sort(names);
+
+			var scores = names.Select((s, i) => (i + 1) * s.Select(c => c - 64).Sum());
+
+			Console.WriteLine(questions);
+			Console.WriteLine(scores.Sum());
+			Console.ReadLine();
+		}
+	}
+}
